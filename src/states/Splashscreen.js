228e9deb4903e893e1e689836d79fa163044d042
mrpapercut <mischa.rodermond@gmail.com>
2016-05-02 14:50:12 +0200

    Adding text to splashscreen

--- src/states/Splashscreen.js
+++ src/states/Splashscreen.js
@@ -6,18 +6,26 @@ class Splashscreen {
 	}
 
 	preload() {
-		this.Game.load.spritesheet('bear', 'assets/sprites/bear.png', 22, 24);
+		this.Game.load.image('bearbig', 'assets/images/bear.png');
 	}
 
 	create() {
-		this.logo = this.Game.add.sprite(this.Game.world.centerX, this.Game.world.centerY, 'bear');
+		this.logo = this.Game.add.sprite(this.Game.world.centerX, this.Game.world.centerY, 'bearbig');
 		this.logo.anchor.setTo(0.5, 0.5);
-		this.text = this.Game.add.text(this.Game.world.centerX, this.Game.world.centerY - 100, 'GunBear', {
+
+		this.title = this.Game.add.text(this.Game.world.centerX, this.Game.world.centerY - 100, 'GunBear', {
 			font: 'Comfortaa',
-			fill: '#000',
-			fontSize: 60
+			fontSize: 60,
+			fill: '#000'
 		});
-		this.text.anchor.setTo(0.5, 0.5);
+		this.title.anchor.setTo(0.5, 0.5);
+
+		this.startbutton = this.Game.add.text(this.Game.world.centerX, this.Game.world.centerY + 100, 'Press [space] to start', {
+			font: 'Comfortaa',
+			fontSize: 30,
+			fill: '#000'
+		});
+		this.startbutton.anchor.setTo(0.5, 0.5);
 
 		this.Game.stage.backgroundColor = '#fff';
 	}
